b20b90ff3d46354f9e837df89841b3d0eeb9923a
Kenneth Graham <kenneth.graham@digitalreasoning.com>
2017-08-17 12:55:55 -0500

    Updated documentation on simple reflex agent

--- Sources/aima-swift/SimpleReflexAgent.swift
+++ Sources/aima-swift/SimpleReflexAgent.swift
@@ -1,5 +1,5 @@
 /*
- See Section 2.4, Figure 2.10: Simple Reflex Agent
+ See Section 2.4.2, Figure 2.10: Simple Reflex Agent
  
  A simple reflex agent chooses an action based on the current precept, ignoring the precept history.
  */
